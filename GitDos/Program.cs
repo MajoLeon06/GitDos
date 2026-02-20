94ebc75991f18cb1f2a2ca87c798167135dbb885
MajoLeon06 <majoleonpaz1321@gmail.com>
2026-02-20 11:14:37 -0600

    Update condición y opciones

--- GitDos/Program.cs
+++ GitDos/Program.cs
@@ -1,6 +1,21 @@
 ﻿Console.WriteLine("Ingrese número de 1 a 10");
 int number = int.Parse(Console.ReadLine());
-if (number<0)
+if (number>0)
+{
+    if (number==1)
+    {
+        Console.WriteLine("Bienvenido");
+    }
+    if (number>1 && number <9)
+    {
+        Console.WriteLine("Opciones múltiples");
+    }
+    else
+    {
+        Console.WriteLine("Salida, esperamos que vuelva pronto");
+    }
+}
+else
 {
     Console.WriteLine("Error");
 }
